50baa38d5e3bee396023c97f99e181f25520f20d
Yuta-Iwase <yutabk201@gmail.com>
2018-01-11 09:50:39 +0900

    pointPlot

--- pointPlot/pointPlot.py
+++ pointPlot/pointPlot.py
@@ -1,36 +1,73 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-inputPath = "list_degreeVSstrength.csv"
-logscale = True
-bin_n = 50
-title = "degree VS strength"
-outputName = "degreeVSstrength"
+### ここに入出力情報を打つ
+inputPath = ["not_shuffle.csv","shuffle.csv"]
+outputName = ""
+withLines = True
+lineColor = "black"
+withPoints = True
+pointColors = ["red","blue"]
+logscaleX = False
+logscaleY = False
+title = "biased RW"
+xLabel = r"$\alpha$"
+yLabel = "HS frac"
+#############################
+
+
+
+
+### ここからプログラム内容
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from compiler.ast import flatten
+import os
 import numpy as np
 
-# 読み取りDataFrame形式で返す
-readData = pd.read_csv(inputPath,header=None)
-# (DataFrame形式).value で縦横のラベルを剥がしてndarray形式に変更
-adarrayData = readData.values
-# x,yのデータへ分離
-adarrayDataX = adarrayData[:,0]
-adarrayDataY = adarrayData[:,1]
-# (ndarray形式).tolist() でndarrayをリスト形式へ変換
-x = adarrayDataX.tolist()
-y = adarrayDataY.tolist()
+# データ読み取り
+xList = []
+yList = []
+for i in range(len(inputPath)):
+    # 読み取りDataFrame形式で返す
+    readData = pd.read_csv(inputPath[i],header=None)
+    # (DataFrame形式).value で縦横のラベルを剥がしてndarray形式に変更
+    adarrayData = readData.values
+    # x,yのデータへ分離
+    adarrayDataX = adarrayData[:,0]
+    adarrayDataY = adarrayData[:,1]
+    # (ndarray形式).tolist() でndarrayをリスト形式へ変換
+    x = adarrayDataX.tolist()
+    xList.append(x)
+    y = adarrayDataY.tolist()
+    yList.append(y)
 
 
-
+# プロット
+width = 0
+if withLines:
+    width = 2
+if logscaleX:
+    plt.xscale("log")
+if logscaleY:
+    plt.yscale("log")
 plt.title(title)
-plt.xlabel("degree")
-plt.ylabel("strength")
-plt.xscale("log")
-plt.yscale("log")
-plt.plot(x,y,linewidth=0,marker="o",fillstyle="none",markersize="10",markeredgecolor="blue")
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
 
+for i in range(len(inputPath)):
+    legend = inputPath[i][:inputPath[i].find(".csv")]
+    if withPoints:
+        plt.plot(xList[i],yList[i],linewidth=width,color=lineColor,marker="o",fillstyle="none",markersize="10",markeredgecolor=pointColors[i],label=legend)
+    else:
+        plt.plot(xList[i],yList[i],linewidth=width,color=lineColor,label=legend)
+plt.legend()
+
+# 書き込み
+if len(outputName)<=0:
+    outputName = inputPath[0][:inputPath[0].find(".csv")]
+if (not os.path.exists("output")):
+    os.makedirs("output")
 plt.savefig("output/" + outputName + ".png")
-plt.savefig("output/" + outputName + ".eps")
+plt.savefig("output/" + outputName + ".eps", transparent=True)
+plt.savefig("output/" + outputName + ".pdf", transparent=True)
