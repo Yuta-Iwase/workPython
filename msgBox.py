51a1bba90c69c6d27f5598b8e085de80db30476f
Owner <yutabk201@gmail.com>
2018-10-23 03:12:24 +0900

    add msgbox

---
+++ msgBox.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+
+# シャープ(#)でコメント
+# 1行目の「 -*- coding: utf-8 -*-」はこのコードはutf-8で書いてあることの宣言。pythonコードは慣例的に1行目にこれを入れる。
+
+# ここからプログラム本文
+# メッセージボックスのために必要なライブラリを連れてくる。
+import tkMessageBox
+# インポートしたライブラリはjavaのオブジェクト感覚でメソッドを呼び出せる。
+tkMessageBox.showinfo('showinfo','Hello python!')
